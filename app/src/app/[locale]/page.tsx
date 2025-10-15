f1bfd39484ef8b3f75a8e62e58c87cfe575b563a
louisdevzz <vohuunhan1310@gmail.com>
2025-10-15 14:28:12 +0700

    feat: add textribbon

--- app/src/app/[locale]/page.tsx
+++ app/src/app/[locale]/page.tsx
@@ -5,7 +5,6 @@ import TextRibbon from "@/components/TextRibbon";
 import Hero from "@/components/Hero";
 import Stats from "@/components/Stats";
 import Programs from "@/components/Programs";
-import Footer from "@/components/Footer";
 import { motion } from "framer-motion";
 
 const Home = () => {
@@ -15,6 +14,7 @@ const Home = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8, delay: 0.2 }}
     >
+      <TextRibbon/>
       <Hero />
       <Stats />
       <Programs />
